caeff9e52cb84479d67bdab14bf91c829f540957
Lauren Welsh <lwelsh@sorensonmedia.com>
2018-08-01 23:30:02 +0100

    refactoring: inline style into css files, reusable templates into components

--- src/app/bridal-party/masters-of-ceremony/masters-of-ceremony.component.ts
+++ src/app/bridal-party/masters-of-ceremony/masters-of-ceremony.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Role {
+  name: string;
+  description: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-masters-of-ceremony',
   templateUrl: './masters-of-ceremony.component.html',
   styleUrls: ['./masters-of-ceremony.component.css']
 })
 export class MastersOfCeremonyComponent implements OnInit {
+  public ceremonies: Array<Role> = [
+    {
+      image: 'assets/Richard.jpg',
+      name: 'Richard Bennett',
+      description: 'Friend of the Bride'
+    },
+    {
+      image: 'assets/Scott.jpg',
+      name: 'Scott Carter',
+      description: 'Friend of the Groom'
+    }
+  ];
 
   constructor() { }
 
